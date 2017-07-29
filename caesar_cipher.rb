1b662b09aaf47adfbe3f8d4f2ed9851bcebcc10e
Sean Fitzgerald <Sean.R.Fitzgerald@outlook.com>
2017-07-29 13:56:55 -0500

    Adds caesar cipher method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,35 @@
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/reloader' if development?
+
+def caesar_cipher(str, num)
+	final_str = ""
+	str.each_char do |character|
+		if !character.match(/[A-Z]|[a-z]/)
+			final_str += character
+			next
+		end
+
+		alph_index = character.ord + num
+		if character.match(/[A-Z]/)
+			while alph_index > 90
+				alph_index -= 26
+			end
+		else
+			while alph_index > 122
+				alph_index -= 26
+			end
+		end
+
+		final_str += alph_index.chr
+	end
+	final_str
+end
 
 get '/' do
 	original_text = params["words"]
-	encrypted_text = 'enter caesar_cipher method here'
+	cipher_number = params["cipher_number"].to_i
+	encrypted_text = caesar_cipher(original_text,cipher_number)
 	erb :index, :locals => {:original_text => original_text,
-							:encrypted_text => encrypted_text}
+							:encrypted_text => encrypted_text,
+							:cipher_number => cipher_number}
 end
